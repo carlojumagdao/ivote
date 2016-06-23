0bbb82e7170b5f8423d42a6b566a0911ad304c89
jobelles <ordiz663@gmail.com>
2016-06-24 04:52:28 +0800

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -155,24 +155,6 @@ Route::post('/settings/party/delete', array(
 // Party Affiliation //
 	// Position //
 
-	// Party Affiliation //
-Route::get('/settings/party', array(
-	'uses' => 'partyController@index',
-	'as' => 'party.index'
-));
-Route::post('/settings/party/add', array(
-	'uses' => 'partyController@add',
-	'as' => 'party.add'
-));
-Route::post('/settings/party/update', array(
-	'uses' => 'partyController@update',
-	'as' => 'party.update'
-));
-Route::post('/settings/party/delete', array(
-	'uses' => 'partyController@delete',
-	'as' => 'party.delete'
-));
-// Party Affiliation //
 });
 
 
@@ -193,87 +175,5 @@ Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 
 
-// Member //
-Route::get('/member', array(
-	'uses' => 'formController@index',
-	'as' => 'member.index'
-));
-Route::get('member/create', array(
-	'uses' => 'formController@create',
-	'as' => 'member.create'
-));
-Route::post('member/add', array(
-	'uses' => 'formController@add',
-	'as' => 'member.add'
-));
-Route::get('member/edit/{id}', array(
-	'uses' => 'formController@edit',
-	'as' => 'member.edit'
-));
-Route::post('member/edit/update', array(
-	'uses' => 'formController@update',
-	'as' => 'member.update'
-));
-Route::post('member/delete', array(
-	'uses' => 'formController@delete',
-	'as' => 'member.delete'
-));
-Route::post('member/email', array(
-    'uses' => 'MailController@send',
-    'as' => 'member.email'
-));
-// Member //
 
-// General Setting //
-Route::get('/settings/general', function () {
-    return view('Settings.general');
-});
-Route::get('/settings/general', array(
-	'uses' => 'gensetController@index',
-	'as' => 'genset.index'
-));
-Route::post('/settings/general/save', array(
-	'uses' => 'gensetController@save',
-	'as' => 'genset.save'
-));
-// General Setting //
-
-
-// Security Questions //
-Route::get('/settings/security', array(
-	'uses' => 'securityController@index',
-	'as' => 'security.index'
-));
-Route::post('/settings/security/add', array(
-	'uses' => 'securityController@add',
-	'as' => 'security.add'
-));
-Route::post('/settings/security/update', array(
-	'uses' => 'securityController@update',
-	'as' => 'security.update'
-));
-Route::post('/settings/security/delete', array(
-	'uses' => 'securityController@delete',
-	'as' => 'security.delete'
-));
-// Security Questions //
-
-
-
-
-
-// Position //
-Route::get('/position', array(
-	'uses' => 'positionController@index',
-	'as' => 'position.index'
-));
-Route::get('/position/create', array(
-	'uses' => 'positionController@create',
-	'as' => 'position.create'
-));
-Route::post('/position/add', array(
-	'uses' => 'positionController@add',
-	'as' => 'position.add'
-));
-// Position //
 
