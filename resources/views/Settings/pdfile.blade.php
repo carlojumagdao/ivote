9066daf9a2d5c1fae7cd8d25d0cd33d2e18e82c2
Nomseo27 <mpvelasco27@outlook.ph>
2016-07-05 01:31:56 +0800

    pdf image

--- resources/views/Settings/pdfile.blade.php
+++ resources/views/Settings/pdfile.blade.php
@@ -7,13 +7,13 @@
 </head>
 <body>
 	<table>
-			<thead> Testing Pdf</thead>
+			<thead></thead>
 
 			<tbody>
 				@foreach($result as $row)
 					<tr>
 						<td>{{$row->strHeader}}</td>
-						<td>{{$row->txtSetLogo}}</td>
+						<td><img src="assets/images/{{$row->txtSetLogo}}" style="max-width: 200px"></td>
 						
 					</tr>
 				@endforeach
