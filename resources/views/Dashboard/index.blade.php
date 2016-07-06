73d1d66af341a2cbd4c1de08593f5cf64e279052
wendellclarete <wendellclarete@gmail.com>
2016-07-06 12:19:57 +0800

    Dashboard7/6

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -28,45 +28,91 @@
             </div>
         @endif
     </div>
-    
-    <div class="col-md-3" >
-        <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="glyphicon glyphicon-user"></i></span>
 
-            <div class="info-box-content">
-              <span class="info-box-text">Total Number of Position</span>
-              <span class="info-box-number">{{$TotalPosition}}</span>
+    <div class="container">
+        <div class="row" >
+            <div class="col-md-2">
+                <div class="col-md-4 panel" style="border-left:8px solid #2c8798; width:260px; height:100px">
+                    <div class="panel-body">
+                        <div class="info-box-number text-center" style="font-size:200%">{{$TotalPosition}}</div>
+                        <div class="info-box-text text-center" style="font-size:130%">Position</div>
+                    </div>
+                </div>
             </div>
-            <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-    </div>
 
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-blue"><i class="glyphicon glyphicon-user"></i></span>
+            <div class="col-md-2 col-sm-offset-1">
+                <div class="col-md-4 panel" style="border-left:8px solid #b22222; width:260px; height:100px">
+                    <div class="panel-body"> 
+                        <div class="info-box-number text-center" style="font-size:200%">{{$TotalCandidate}}</div>
+                        <div class="info-box-text text-center" style="font-size:130%">Candidate</div>
+                    </div>
+                </div>
+            </div> 
 
-            <div class="info-box-content">
-              <span class="info-box-text">Total Number of Candidate</span>
-              <span class="info-box-number">{{$TotalCandidate}}</span>
+            <div class="col-md-2 col-sm-offset-1">
+                <div class="col-md-4 panel" style="border-left: 8px solid #006400; width:260px; height:100px">
+                    <div class="panel-body">  
+                        <div class="info-box-number text-center" style="font-size:200%">{{$TotalVoter}}</div>
+                        <div class="info-box-text text-center" style="font-size:130%">Voter</div>
+                    </div>
+                </div>
             </div>
-            <!--/.info-box-content-->
         </div>
-        <!-- /.info-box-->
     </div> 
 
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="glyphicon glyphicon-user"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Total Number of Voter</span>
-              <span class="info-box-number">{{$TotalVoter}}</span>
+    <div class="col-md-4">
+        <div class="panel" style="width:560px; height:200px;">
+            <div class="panel-heading" style="background-color:#DCE046"><i class="glyphicon glyphicon-calendar"></i>
+                Putangina mag code
             </div>
-            <!-- /.info-box-content -->
+
+            <div class="panel-body">
+                <div>
+                    <span></span>
+                </div>
+                <div class="progress">
+                    <div>
+                    </div>
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                    aria-valuemin="0" aria-valuemax="100" style="width:70%; background-color:#B3B71B">
+                        70%
+                    </div>
+                </div>
+                <div class="progress">
+                    <div>
+                    </div>
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                    aria-valuemin="0" aria-valuemax="100" style="width:80%; background-color:#B3B71B">
+                        80%
+                    </div>
+                </div> 
+                <div class="progress">
+                    <div>
+                    </div>
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                    aria-valuemin="0" aria-valuemax="100" style="width:90%; background-color:#B3B71B">
+                        90%
+                    </div>
+                </div>  
+            </div>
         </div>
-        <!-- /.info-box -->
     </div>
+
+    <div class="col-md-5 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading"><i class="glyphicon glyphicon-calendar"></i> Start Date</div>
+            <div class="panel-body">September 26, 2016</div>    
+        </div>  
+    </div>
+
+    <div class="col-md-5 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading"><i class="glyphicon glyphicon-calendar"></i> End Date</div>
+            <div class="panel-body">September 27, 2016</div>  
+        </div>  
+    </div>
+
+    
     
     
 @stop 
