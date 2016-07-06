f0bedaeac021a37f7618fc809e5eb915fb297dba
Nomseo27 <mpvelasco27@outlook.ph>
2016-07-06 15:03:14 +0800

    Countdown Changes

--- app/Http/Controllers/CountdownController.php
+++ app/Http/Controllers/CountdownController.php
@@ -12,7 +12,7 @@ class CountdownController extends Controller
     public function Count(){
     	$GenSet = GenSet::find(1);
     	$start = date_create( $GenSet->datSetStart);
-    	$end = date_create($GenSet->datSetEnd);
+    	$end = date_create(date("Y-m-d"));
     	$day = date_diff($start, $end);
     	return view('Countdown', ['day'=>$day->days]);
     }
