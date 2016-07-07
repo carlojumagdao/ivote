fda165f5a7c14970faa24d8458d48555c8e3250a
Nomseo27 <mpvelasco27@outlook.ph>
2016-07-07 23:26:20 +0800

    Countdown Fixes and Config Timezone Change
    Timezone changed to Philippine time

--- app/Http/Controllers/CountdownController.php
+++ app/Http/Controllers/CountdownController.php
@@ -12,7 +12,7 @@ class CountdownController extends Controller
     public function Count(){
     	$GenSet = GenSet::find(1);
     	$start = date_create($GenSet->datSetStart);
-        $now = date_create(date("Y-m-d H:m:s"));
+        $now = date_create(date("Y-m-d H:i:s"));
     	//$now = date_create(date());
     	$day = date_diff($now, $start);
         $sec = ($day->d * 24 * 3600) + ($day->h * 3600) + ($day->i * 60) + $day->s;
